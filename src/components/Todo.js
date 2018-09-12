9985945bbc7ef1bd994d42ce4b941316a4ec6d5c
budougumi0617 <budougumi0617@gmail.com>
2018-09-13 00:51:47 +0900

    :shirt: Fix lint

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,23 +3,24 @@
 import * as React from 'react';
 
 type Props = {
-  onClick: (item: {}, e:  Event) => void,
+  onClick: (item: {}, e: Event) => void,
   completed: boolean,
   text: string
 };
 
-type State={}
+type State = {};
 
-export default  class Todo extends React.Component<Props, State> {
+export default class Todo extends React.Component<Props, State> {
   render() {
     return (
-  <li
-    onClick={this.props.onClick}
-    style={ {
-      textDecoration: this.props.completed ? 'line-through' : 'none'
-    }}
-  >
-    {this.props.text}
-  </li>
-)};
-};
+      <li
+        onClick={this.props.onClick}
+        style={{
+          textDecoration: this.props.completed ? 'line-through' : 'none'
+        }}
+      >
+        {this.props.text}
+      </li>
+    );
+  }
+}
