8abd25b0cf3a95049175901c0548c5f8c8f46b86
budougumi0617 <budougumi0617@gmail.com>
2018-09-17 09:20:45 +0900

    :+1: Add reducers

---
+++ src/reducers/count.js
@@ -0,0 +1,20 @@
+// @flow
+
+import type { Action } from '../actions';
+
+export type State = {
+  value: number
+};
+
+const initialState = { value: 0 };
+
+export default (state: State = initialState, action: Action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { value: state.value + 1 };
+    case 'DECREMENT':
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+};
