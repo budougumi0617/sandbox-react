a8bd6fa0754edd0ff2c55308a0a1f8d551c961a7
budougumi0617 <budougumi0617@gmail.com>
2018-11-04 16:15:46 +0900

    :+1: Ignore process

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,37 @@
 module.exports = {
-  'extends': [
-    'eslint:recommended',
-    'plugin:flowtype/recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-    'prettier/flowtype',
-    'prettier/react'
+  extends: [
+    "eslint:recommended",
+    "plugin:flowtype/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "prettier/flowtype",
+    "prettier/react"
   ],
-  'env': {
-    'browser': true
+  env: {
+    browser: true
   },
-  'plugins': ['react', 'flowtype', 'prettier', 'jsx-a11y'],
-  'rules': {
-    'prettier/prettier': [
-      'error',
+  plugins: ["react", "flowtype", "prettier", "jsx-a11y"],
+  rules: {
+    "prettier/prettier": [
+      "error",
       {
-        'singleQuote': true,
-        'printWidth': 100
+        singleQuote: true,
+        printWidth: 100
       }
     ],
-    'yoda': 0,
-    'no-unused-vars': 1,
-    'react/prefer-stateless-function': 0,
-    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx']  }],
-    'jsx-a11y/href-no-hash': 'off',
-    'jsx-a11y/anchor-is-valid': ['warn', { 'aspects': ['invalidHref'] }]
+    yoda: 0,
+    "no-unused-vars": 1,
+    "react/prefer-stateless-function": 0,
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "jsx-a11y/href-no-hash": "off",
+    "jsx-a11y/anchor-is-valid": ["warn", { aspects: ["invalidHref"] }]
   },
-  'globals': {
-    '$': false,
-    'describe': true,
-    'it': true,
-    'test': true,
-    'expect': true
+  globals: {
+    $: false,
+    describe: true,
+    it: true,
+    test: true,
+    expect: true,
+    process: true
   }
-}
+};
