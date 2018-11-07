0e22b95d69e1ada152f4c3b5c2648660dd204979
budougumi0617 <budougumi0617@gmail.com>
2018-11-07 09:25:04 +0900

    :recycle: Remove block statement

--- src/actions/index.js
+++ src/actions/index.js
@@ -49,9 +49,8 @@ export const readEvent: any => ReadEventAction = createAction(READ_EVENT, (id: s
   })
 );
 
-export const deleteEvent: any => DeleteEventAction = createAction(DELETE_EVENT, (id: string) => {
-  // FIXME DELETEのときだけreturnしないとpayloadが入らない。
-  return axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRING}`).then(response => {
+export const deleteEvent: any => DeleteEventAction = createAction(DELETE_EVENT, (id: string) =>
+  axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRING}`).then(response => {
     return response.data;
-  });
-});
+  })
+);
